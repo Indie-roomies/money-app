c549f6ad2c8270230e8a7dddc91fae51c82c86e9
d4niel-olaya <olayajuandaniel8@gmail.com>
2023-08-01 22:45:39 -0500

    base

---
+++ wwwroot/js/JavaScript.js
@@ -0,0 +1,10 @@
+﻿function baseFunction() {
+    var appBase = document.createElement("base");
+    console.log(appBase.href.indexOf("localhost"))
+    if (window.location.href.indexOf("localhost") != -1) {
+        appBase.href = "/";
+    }
+}
+
+
+baseFunction();
