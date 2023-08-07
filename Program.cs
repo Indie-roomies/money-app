16fd1241ef650a33bcfb364ea0bc91d8a99a004b
d4niel-olaya <olayajuandaniel8@gmail.com>
2023-08-06 22:40:31 -0500

    singleton

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using MatBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using money_app;
+using moneyapp.Services;
 
 internal class Program
 {
@@ -13,7 +14,7 @@ internal class Program
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+        builder.Services.AddSingleton<ITaskService, TaskService>();
         await builder.Build().RunAsync();
     }
 }
